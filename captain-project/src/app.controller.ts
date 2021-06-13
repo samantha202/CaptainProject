8a050f7dd13d22f81a9c440b80bf9f4c40182f55
samantha202 <nsamanthaduclaire@gmail.com>
2021-06-14 00:12:47 +0200

    controller app config

--- captain-project/src/app.controller.ts
+++ captain-project/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import {PrismaService} from 'prisma/prisma.services';
+import { Post} from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+              private readonly prismaService: PrismaService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('posts')
+  async post(): Promise<Post[]>{
+    return this.prismaService.post.findMany();
+  }
+  @Get('post/:id')
+  async posts(@Param('id') id: string): Promise<Post>{
+    return this.prismaService.post.findUnique({where: {id: +id}});
   }
 }
